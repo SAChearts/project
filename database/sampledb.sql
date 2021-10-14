15023022a7c988a731acbeb79d15621d1a2a822c
Vincent Martinez <vincentem20@gmail.com>
2021-10-13 20:42:23 -0500

    update sql script

--- database/sampledb.sql
+++ database/sampledb.sql
@@ -1,9 +1,9 @@
-DROP TABLE comments IF EXISTS;
-DROP TABLE incedents IF EXISTS;
-DROP TABLE punctuality IF EXISTS;
-DROP TABLE department IF EXISTS;
-DROP TABLE employees IF EXISTS;
-DROP TABLE company IF EXISTS;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS incedents;
+DROP TABLE IF EXISTS punctuality;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS company;
 
 CREATE TABLE employees (
     empID           INTEGER PRIMARY KEY,
@@ -14,45 +14,43 @@ CREATE TABLE employees (
     dob             TEXT NOT NULL,
     startdate       TEXT NOT NULL,
     managerID       INTEGER,
-    CONSTRAINT employees_manager_fk FOREIGN KEY (managerID)
-        REFERENCES employees(managerID)
+    FOREIGN KEY (managerID) REFERENCES employees(managerID)
 );
 
-CREATE TABLE department {
+CREATE TABLE department (
     deptID          INTEGER PRIMARY KEY,
     dept_name       TEXT NOT NULL,
     managerID       INTEGER NOT NULL,
     proj_goal       INTEGER,
     punc_goal       INTEGER,
     incedent_goal   INTEGER,
-    CONSTRAINT department_manager_fk FOREIGN KEY (managerID)
-        REFERENCES employees(managerID)
+    FOREIGN KEY (managerID) REFERENCES employees(managerID)
 
-};
+);
 
 CREATE TABLE company (
     co_name    VARCHAR(100) PRIMARY KEY,
     co_mission  TEXT,
-    co_tagline: TEXT
+    co_tagline TEXT
     
 );
 
 CREATE TABLE comments (
     empID       INTEGER,
     comments    TEXT,
-    magnitude   INTEGER,
+    magnitude   INTEGER
 );
 
 CREATE TABLE punctuality (
     empID           INTEGER,
     incedent_date   TEXT,
     incedent_type   TEXT,
-    magnitude       INTEGER,
+    magnitude       INTEGER
 );
 
 CREATE TABLE incedents (
     empID           INTEGER,
     incedent_date   TEXT,
     incedent_type   TEXT,
-    magnitude       INTEGER,
+    magnitude       INTEGER
 );
