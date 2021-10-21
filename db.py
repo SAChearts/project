44f6cb139f8661ddae0705e7ac60caecaee4600e
Vincent Martinez <vincentem20@gmail.com>
2021-10-21 14:00:44 -0500

    add db connection

--- db.py
+++ db.py
@@ -1,43 +1,32 @@
 import sqlite3
-
-import click
-from flask import current_app, g
-from flask.cli import with_appcontext
+import hashlib
 
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
+def db_connect():
+    return sqlite3.connect('database/sample.db')
 
 
-def close_db(e=None):
-    db = g.pop('db', None)
+def login(usrname, pword):
+    conn = db_connect()
+    cur = conn.cursor()
 
-    if db is not None:
-        db.close()
+    # Setup query statement
+    statement = "SELECT * FROM employees WHERE username=? AND pass=?"
+    usrname = usrname.strip().lower()
+    pword = pword.rstrip().encode()
+    pword = hashlib.md5(pword).hexdigest()  # hashed pword
+    vals = (usrname, pword)
+
+    # execute query
+    cur.execute(statement, vals)
+    usr = cur.fetchone()
+    conn.close()
+
+    # TODO Return user data.
+    if usr:
+        return True
+    return False
 
 
-def init_db():
-    db = get_db()
-
-    with current_app.open_resource('sampledb.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-
-
-@click.command('init-db')
-@with_appcontext
-def init_db_command():
-    """Clear the existing data and create new tables."""
-    init_db()
-    click.echo('Initialized the database.')
-
-
-def init_app(app):
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
+# if __name__ == "__main__":
+#     login("lsmith", "lsmith1234")
